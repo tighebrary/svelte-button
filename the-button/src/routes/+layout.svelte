03292d9a71a18a1c41e9dd9dfe0228a132d40aaf
Alex Tighe <alex@tighebrary.io>
2025-12-06 17:41:42 +1100

    Initial publish

---
+++ the-button/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.ico';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ the-button/src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<svelte:options runes={true} />
+<script>
+  import data from '$lib/data.json';
+  
+  let count = $state(0);
+
+  let match = $derived(data.find(d => d.presses === count));
+  
+  let buttonLabel = $state('');
+  $effect(() => {
+    if (match) {
+      buttonLabel = match.text;
+    }
+    else {
+      buttonLabel = `You've pressed ${count} times`;
+    }
+  });
+  
+  function increment() {
+    count += 1
+  }
+</script>
+
+<main>
+  <div>
+    <button onclick={increment}>
+      {buttonLabel}
+    </button>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    font-family: sans-serif;
+  }
+
+  button {
+    font-size: 1.5rem;
+    padding: 1rem 2rem;
+    cursor: pointer;
+    background-color: #ff3e00;
+    color: white;
+    border-color: #ff3e00;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    transition: all 0.2s;
+    user-select: none;
+    white-space: pre-wrap; 
+  }
+
+  @keyframes fade {
+    from { opacity: 0; transform: translateY(-10px); }
+    to { opacity: 1; transform: translateY(0); }
+  }
+</style>
